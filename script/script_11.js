cb99a9872563f476e137f3189e84b4270b7e3ebe
THINKERS-PVT <147609967+THINKERS-PVT@users.noreply.github.com>
2023-10-13 09:23:36 +0530

    Update script_11.js

--- script/script_11.js
+++ script/script_11.js
@@ -4,7 +4,7 @@ let n_json=[
                  {
                 desc: "We are THINKERS PRIVATE LIMITED,Bridge the gap between researchers and investors by providing researchers with the resources they need to develop their business acumen and write a successful grant proposal, and by connecting researchers with investors who are interested in funding innovative research ",
                 id: 1,
-                image: "https://thinkers-pvt.github.io/images/1%200b5QvAKZ9inGUnOHposD2w.jpg",
+                image: "https://thinkers-pvt.github.io/images-prj/1%200b5QvAKZ9inGUnOHposD2w.jpg",
                 title: "THINKERS PRIVATE LIMITED",
                 url: "https://thinkerrs.com/"
             }
